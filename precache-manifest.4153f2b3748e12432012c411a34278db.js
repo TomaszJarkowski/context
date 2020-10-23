2c6c39f328b0a422b1221a9f03ff838d7fb68875
Tomasz Jarkowski <tomaszjarkowski1998@gmail.com>
2020-10-23 13:04:29 +0200

    Updates

---
+++ precache-manifest.4153f2b3748e12432012c411a34278db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad9aa554523f37e2f1791df904b78a4",
+    "url": "/context/index.html"
+  },
+  {
+    "revision": "a7828f11f303456f3c55",
+    "url": "/context/static/css/main.2ac0bfeb.chunk.css"
+  },
+  {
+    "revision": "7ba41f38a2896c2db4bd",
+    "url": "/context/static/js/2.be5bdcbd.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/context/static/js/2.be5bdcbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7828f11f303456f3c55",
+    "url": "/context/static/js/main.eaae4db1.chunk.js"
+  },
+  {
+    "revision": "821c5c3040ee082e03d9",
+    "url": "/context/static/js/runtime-main.20de9f4e.js"
+  }
+]);
